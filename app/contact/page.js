d5eecb8ae31b1e566473857c922001b64047918f
Manohar Kale <manoharkale5368@gmail.com>
2025-01-02 10:42:34 +0530

    add metadata

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function page() {
-  return (
-    <div>
-      I am the contact page
-    </div>
-  )
+  return <div>I am the contact page</div>;
 }
 
-export default page
+export default page;
+
+export const metadata = {
+  title: "Contact Page",
+  description: "This is the contact page"
+};
