d5c07789e947a72bfdd2bc79ce901750e7a9be39
Manohar Kale <manoharkale5368@gmail.com>
2025-01-02 12:23:21 +0530

    add api

--- app/api/add/route.js
+++ app/api/add/route.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 
 async function POST(Request) {
   let data = await Request.json();
+  console.log(data);
   return NextResponse.json({ success: true, data });
 }
 
