205256b30a8a61618474fa328d498da29b4c2e54
Manohar Kale <manoharkale5368@gmail.com>
2025-01-02 12:20:38 +0530

    add api

--- app/about/page.js
+++ app/about/page.js
@@ -1,28 +1,11 @@
-"use client";
-import React from "react";
+import React from 'react'
 
 function page() {
-  const handleclick = async () => {
-    const data = { name: "john", age: 30 };
-
-    let res = await fetch("http://localhost:3000/api/add", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    let resData = await res.json();
-    console.log(resData);
-  };
-
   return (
-    <>
-      <div>API request</div>
-      <button onClick={handleclick}>click me</button>
-    </>
-  );
+    <div>
+      This is the about page
+    </div>
+  )
 }
 
-export default page;
+export default page
