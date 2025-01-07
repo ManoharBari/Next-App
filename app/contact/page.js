fcbc10c93cf7cc01879769279e46365541d8c382
Manohar Kale <manoharkale5368@gmail.com>
2025-01-07 14:45:15 +0530

    ISR and SSR

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,12 +1,21 @@
 import React from "react";
 
-function page() {
-  return <div>I am the contact page</div>;
+async function page() {
+  const data = await fetch("https://api.vercel.app/blog");
+  const posts = await data.json();
+
+  return (
+    <>
+      <h2>SSR, SSG and ISG/ISR</h2>
+      <br />
+      {posts.map((post) => (
+        <div>
+          <h5>{post.title}</h5>
+          <p>{post.content}</p>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default page;
-
-export const metadata = {
-  title: "Contact Page",
-  description: "This is the contact page"
-};
