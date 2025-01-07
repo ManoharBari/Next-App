7e7062874b0d6cb054f01e90b756b3cbd87e05cd
Manohar Kale <manoharkale5368@gmail.com>
2025-01-07 15:47:21 +0530

    ISR and SSR

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,19 +1,34 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
-async function page() {
-  const data = await fetch("https://api.vercel.app/blog", {
-    revalidate: 3600, 
-  });
-  const posts = await data.json();
+function page() {
+  const [post, setpost] = useState([])
+
+  async function click() {
+    const data = await fetch("https://api.vercel.app/blog");
+    const posts = await data.json();
+    setpost(posts)
+  }
 
   return (
     <>
       <h2>SSR, SSG and ISG/ISR</h2>
       <br />
-      {posts.map((post) => (
+      <button onClick={click}>click me</button>
+      {post.map((blog) => (
         <div>
-          <h5>{post.title}</h5>
-          <p>{post.content}</p>
+          <h5>{blog.title}</h5>
+          <p>{blog.content}</p>
+          <style jsx global>
+            {`
+              h5 {
+                color: red;
+              }
+              p {
+                color: yellow;
+              }
+            `}
+          </style>
         </div>
       ))}
     </>
